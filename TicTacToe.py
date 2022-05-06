c0bfd1b1ec0261a20d59e6a1d537f2f6507d7439
TheChupacabrah <98102649+TheChupacabrah@users.noreply.github.com>
2022-05-06 15:03:05 -0400

    Add files via upload

---
+++ TicTacToe.py
@@ -0,0 +1,124 @@
+import random
+
+
+class TicTacToe:
+
+    def __init__(self):
+        self.board = []
+
+    def create_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append('-')
+            self.board.append(row)
+
+    def get_random_first_player(self):
+        return random.randint(0, 1)
+
+    def replace(self, row, col, player):
+        self.board[row][col] = player
+
+    def winner(self, player):
+        win = None
+
+        n = len(self.board)
+
+        # checking rows
+        for i in range(n):
+            win = True
+            for j in range(n):
+                if self.board[i][j] != player:
+                    win = False
+                    break
+            if win:
+                return win
+
+        # checking columns
+        for i in range(n):
+            win = True
+            for j in range(n):
+                if self.board[j][i] != player:
+                    win = False
+                    break
+            if win:
+                return win
+
+        # checking diagonals
+        win = True
+        for i in range(n):
+            if self.board[i][i] != player:
+                win = False
+                break
+        if win:
+            return win
+
+        win = True
+        for i in range(n):
+            if self.board[i][n - 1 - i] != player:
+                win = False
+                break
+        if win:
+            return win
+        return False
+
+        for row in self.board:
+            for item in row:
+                if item == '-':
+                    return False
+        return True
+
+    def check_board(self):
+        for row in self.board:
+            for item in row:
+                if item == '-':
+                    return False
+        return True
+
+    def swap_player_turn(self, player):
+        return 'X' if player == 'O' else 'O'
+
+    def show_board(self):
+        for row in self.board:
+            for item in row:
+                print(item, end=" ")
+            print()
+
+    def start(self):
+        self.create_board()
+
+        player = 'X' if self.get_random_first_player() == 1 else 'O'
+        while True:
+            print(f"Player {player} turn")
+
+            self.show_board()
+
+            # taking user input
+            row, col = list(
+                map(int, input("Enter row and column numbers (separate with a space): ").split()))
+            print()
+
+            # change board spot
+            self.replace(row - 1, col - 1, player)
+
+            # win checker for current player
+            if self.winner(player):
+                print(f"Player {player} stomped on yo face!")
+                break
+
+            # checking for draw
+            if self.check_board():
+                print("The cat ate it!")
+                break
+
+            # change turns
+            player = self.swap_player_turn(player)
+
+        # final board
+        print()
+        self.show_board()
+
+
+# starting the game
+tic_tac_toe = TicTacToe()
+tic_tac_toe.start()
